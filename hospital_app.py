53822b7a1b0fcce84cd0dcd4c8841b8034e38fbf
willworst <kullzep@gmail.com>
2026-06-30 11:13:08 +0700

    Update and rename hospital_app_Template.py to hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,7 +1,12 @@
 #Importing Libraries
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
 #Page Title
+set.page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
 #HTML and CSS
 st.markdown("""
@@ -35,6 +40,22 @@ div[data-testid="stCheckbox"] label {
 
 @st.cache_resource
 #Load the Model
+def load_model():
+    with open('hospital_model.pkl', 'rb') as f:
+        return pickle.load(f)
+
+bundle        = load_model()
+model         = bundle['model']
+scaler        = bundle['scaler']
+features      = bundle['features']
+cols_to_scale = bundle['cols_to_scale']
+dept_map_inv  = bundle['dept_map_inv']
+gender_map    = bundle['gender_map']
+temp_map      = bundle['temp_map']
+hr_map        = bundle['hr_map']
+dur_map       = bundle['dur_map']
+cc_map        = bundle['cc_map']
+
 
 DEPT_INFO = {
     'Respiratory Medicine': {
